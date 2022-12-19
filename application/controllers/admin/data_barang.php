99bfb233a7ea8abded3cd8b18ff2578c0df6f32e
septianaatia <septianatia099@gmail.com>
2022-12-19 16:04:06 +0700

    add databarang

---
+++ application/controllers/admin/data_barang.php
@@ -0,0 +1,11 @@
+<?php
+
+class Data_barang Extends CI_Controller{
+    public function index()
+    {
+        $this->load->view('templates_admin/header');
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/data_barang');
+        $this->load->view('templates_admin/footer');
+    }
+}
---
+++ application/views/admin/data_barang.php
@@ -0,0 +1,24 @@
+<div class="container-fluid">
+    <button class="btn btn-sm btn-primary"><i class="fas fa-plus fa-sm"></i>Tambah Barang</button>
+
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Nama Barang</th>
+            <th>Keterangan</th>
+            <th>kategori</th>
+            <th>Harga</th>
+            <th>Stok</th>
+            <th colspan="3">AKSI</th>  
+        </tr>
+
+        <?php
+        $no=1;
+        foreach($barang as $brg) : ?>
+
+        <tr>
+            <td><?php echo $no== ?>
+
+        <?php endforeach; ?>
+
+</div>
