aa278964a9eb3cde7ca1a63eb3a3a7c215816640
septianaatia <septianatia099@gmail.com>
2022-12-19 16:18:11 +0700

    #

--- application/views/admin/data_barang.php
+++ application/views/admin/data_barang.php
@@ -17,7 +17,18 @@
         foreach($barang as $brg) : ?>
 
         <tr>
-            <td><?php echo $no== ?>
+            <td><?php echo $no== ?></td>
+            <td><?php echo $brg->nama_brg ?></td>
+            <td><?php echo $brg->keterangan ?></td>
+            <td><?php echo $brg->kategori ?></td>
+            <td><?php echo $brg->harga ?></td>
+            <td><?php echo $brg->stok ?></td>
+            <td><div class="btn btn-success btn-sm"><i class="fas fa-search-plus"></i></div></td>
+            <td><div class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></div></td>
+            <td><div class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></div></td>
+            
+        </tr>
+
 
         <?php endforeach; ?>
 
